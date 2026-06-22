cc032e90817d042dd15f66aa88ad29dd67796a08
Srilakshman K <srilogan2612004@gmail.com>
2026-06-22 11:13:53 +0530

    React Vite and Tailwind css setup were completed in the frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+
+
+function App() {
+ 
+
+  return (
+   <div className="min-h-screen bg-linear-to-br from-zinc-950 via-slate-900 to-indigo-950 flex items-center justify-center relative overflow-hidden"></div>
+  )
+}
+
+export default App
