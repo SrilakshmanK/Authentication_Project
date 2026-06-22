83b601e6005caa10ab57378ec34f481f77d2a70f
Srilakshman K <srilogan2612004@gmail.com>
2026-06-22 12:27:33 +0530

    Background and Floating Shapes were added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,14 @@
-
+import FloatingShape from "./components/FloatingShape.jsx"
 
 function App() {
  
 
   return (
-   <div className="min-h-screen bg-linear-to-br from-zinc-950 via-slate-900 to-indigo-950 flex items-center justify-center relative overflow-hidden"></div>
+   <div className="min-h-screen bg-linear-to-br from-zinc-950 via-slate-900 to-indigo-950 flex items-center justify-center relative overflow-hidden">
+      <FloatingShape color="bg-indigo-500" size="w-64 h-64" top="-5%" left="-10%" delay={0} />
+    <FloatingShape color="bg-violet-500" size="w-48 h-48" top="70%" left="80%" delay={5} />
+    <FloatingShape color="bg-fuchsia-500" size="w-32 h-32" top="40%" left="-10%" delay={2} />
+   </div>
   )
 }
 
