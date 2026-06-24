fce54688895c399ec893d9f21a6f8e40beeefb90
Srilakshman K <srilogan2612004@gmail.com>
2026-06-25 05:15:31 +0530

    Login functionality were added successfully

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,27 +1,44 @@
 import { useState } from "react";
-import {motion} from 'framer-motion';
-import { Mail,Lock, Loader, Eye , EyeOff ,LoaderCircleIcon } from "lucide-react";
-import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
+import {
+  Mail,
+  Lock,
+  Loader,
+  Eye,
+  EyeOff,
+  LoaderCircleIcon,
+} from "lucide-react";
+import { Link, useNavigate } from "react-router-dom";
+import { useAuthStore } from "../store/authStore";
 
 import Input from "../components/Input";
-
 
 const LoginPage = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [showPassword, setShowPassword] = useState(false);
-  const isLoading = false;
-  const handleLogin = (e) => {
-    e.preventDefault();
-  }
+  
+  const { login, error, isLoading } = useAuthStore();
+  const navigate = useNavigate();
 
-  return (  <motion.div
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      await login(email, password);
+      navigate("/");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <motion.div
       initial={{ opacity: 0, y: 20 }}
       animate={{ opacity: 1, y: 0 }}
       transition={{ duration: 0.5 }}
       className="max-w-md w-full bg-slate-900/50 bg-opacity-50 backdrop-filter backdrop-blur-xl rounded-2xl shadow-xl overflow-hidden "
     >
-     <div className="p-8">
+      <div className="p-8">
         <h2 className="text-4xl font-light tracking-wide text-white text-center mb-2">
           Welcome Back
         </h2>
@@ -29,8 +46,8 @@ const LoginPage = () => {
         <p className="text-center text-slate-400 text-sm mb-6">
           Sign in to continue your journey.
         </p>
-         <form onSubmit={handleLogin}>
-            <Input
+        <form onSubmit={handleLogin}>
+          <Input
             icon={Mail}
             type="email"
             placeholder="Email Address"
@@ -47,12 +64,15 @@ const LoginPage = () => {
             onRightIconClick={() => setShowPassword(!showPassword)}
           />
           <div className="flex items-center mb-6">
-            <Link to="/forgot-password" className="text-sm text-slate-200 hover:text-white transition-colors duration-200" >
-             Forgot password ?
+            <Link
+              to="/forgot-password"
+              className="text-sm text-slate-200 hover:text-white transition-colors duration-200"
+            >
+              Forgot password ?
             </Link>
           </div>
-
-             <motion.button
+           {error && <p className="text-red-500 font-semibold mt-2">{error}</p>}
+          <motion.button
             className="
         mt-5
         w-full
@@ -76,11 +96,15 @@ const LoginPage = () => {
             type="submit"
             disabled={isLoading}
           >
-          {isLoading?<LoaderCircleIcon className="size-6 animate-spin  mx-auto"/>:"Login"}
+            {isLoading ? (
+              <LoaderCircleIcon className="size-6 animate-spin  mx-auto" />
+            ) : (
+              "Login"
+            )}
           </motion.button>
         </form>
-        </div>
-         <div className="px-8 py-4 border-t border-white/5 flex justify-center bg-gray-900">
+      </div>
+      <div className="px-8 py-4 border-t border-white/5 flex justify-center bg-gray-900">
         <p className="text-sm text-slate-400">
           Don't have an account ?{" "}
           <Link
@@ -90,11 +114,9 @@ const LoginPage = () => {
             SignUp
           </Link>
         </p>
-
-       
       </div>
-
-    </motion.div>);
+    </motion.div>
+  );
 };
 
 export default LoginPage;
--- frontend/src/store/authStore.js
+++ frontend/src/store/authStore.js
@@ -16,13 +16,11 @@ export const useAuthStore = create((set) => ({
     set({ isLoading: true, error: null });
 
     try {
-      const response = await axios.post(
-        `${API_URL}/signup`,{
-          email,
-          password,
-          name,
-        }
-      );
+      const response = await axios.post(`${API_URL}/signup`, {
+        email,
+        password,
+        name,
+      });
       set({
         user: response.data.user,
         isAuthenticated: true,
@@ -37,25 +35,54 @@ export const useAuthStore = create((set) => ({
     }
   },
 
-  verifyEmail:async (code)=>{
+  login: async (email, password) => {
     set({ isLoading: true, error: null });
     try {
-      const response = await axios.post(`${API_URL}/verify-email`,{code});
-      set({user : response.data.user,isAuthenticated:true,isLoading:false});
+      const response = await axios.post(`${API_URL}/login`, {
+        email,
+        password,
+      });
+      set({
+        isAuthenticated: true,
+        user: response.data.user,
+        error: null,
+        isLoading: false,
+      });
     } catch (error) {
-      set({error:error.response.data.message || "Error verifying email", isLoading:false});
+      set({error:error.response?.data?.message || "Error logging in", isLoading:false});
       throw error;
     }
-
   },
-  checkAuth : async () => {
-    set({isCheckingAuth:true,error:null})
+
+  verifyEmail: async (code) => {
+    set({ isLoading: true, error: null });
+    try {
+      const response = await axios.post(`${API_URL}/verify-email`, { code });
+      set({
+        user: response.data.user,
+        isAuthenticated: true,
+        isLoading: false,
+      });
+    } catch (error) {
+      set({
+        error: error.response.data.message || "Error verifying email",
+        isLoading: false,
+      });
+      throw error;
+    }
+  },
+
+  checkAuth: async () => {
+    set({ isCheckingAuth: true, error: null });
     try {
       const response = await axios.get(`${API_URL}/check-auth`);
-      set({user: response.data.user,isAuthenticated:true,isCheckingAuth:false})
+      set({
+        user: response.data.user,
+        isAuthenticated: true,
+        isCheckingAuth: false,
+      });
     } catch (error) {
-      set({error:null,isCheckingAuth:false,isAuthenticated:false})
-      
+      set({ error: null, isCheckingAuth: false, isAuthenticated: false });
     }
-  }
+  },
 }));
